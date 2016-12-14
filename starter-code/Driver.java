1c60e09e257e564c55e0f11eaa3b86a6c5ff8642
dave-scar <david.scarimbolo@gmail.comgit config --global user.email david.scarimbolo@gmail.com>
2016-12-13 19:08:42 -0500

    Lists-Lab

--- starter-code/Driver.java
+++ starter-code/Driver.java
@@ -1,58 +1,89 @@
-package ListLab;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Driver {
-	
-	private static String[] firstNames = {"Josh", "John", "Jason", "Dave", "Mike", "Dan", "Gary", "Frank", "Joe",
-			"Eric", "Bob", "Kyle", "Bill", "Shane", "Andrew", "Jim", "Doug", "Chris", "Katie", 
-			"Kayla", "Sarah", "Elizabeth", "Stef", "Mary", "Jane", "Kelly", "Jessica", "Beth",
-			"Carla", "Becky", "Linda", "Ruth", "Sandra", "Amy", "Claire", "Donna"};
-	
-	private static String[] lastNames = {"Smith", "Davis", "Jones", "Taylor", "Manning", "Jennings", "Williams",
-			"Parker", "Elliot", "Patrick", "Johnson", "Davidson", "O'Connor", "Fritz", "Green",
-			"Gold", "Foley", "Freeman", "Willis", "Grant"};
 
-	public static void main(String[] args) {
-		List<Student> studentArrayList = new ArrayList<>();
-		MyLinkedList<Student> studentLinkedList = new MyLinkedList<>();
-		
-		generateStudents(20, studentArrayList);
-		generateStudents(20, studentLinkedList);
-		
-		//Determine which data structure is fastest for each
-		//operation. Complete the action
-		
-		//Delete the first, if any, Student with the last name of "Smith"
-		
-		//Change the name of the 3rd entry to "Joe Montana"
-		
-		//Remove the 10th element
-	}
-	
-	//To generate the ArrayList
-	private static void generateStudents(int numberOfStudents, List<Student> outList) {
-		Random random = new Random();
-		
-		for(int i = 0; i < numberOfStudents; i++) {
-			int firstNameIndex = random.nextInt(firstNames.length -1);
-			int lastNameIndex = random.nextInt(lastNames.length -1);
-			double gpa = 3.5 * random.nextDouble() + 0.5;
-			outList.add(new Student(firstNames[firstNameIndex], lastNames[lastNameIndex], gpa));	
-		}
-	}
+    private static String[] firstNames = {"Josh", "John", "Jason", "Dave", "Mike", "Dan", "Gary", "Frank", "Joe",
+            "Eric", "Bob", "Kyle", "Bill", "Shane", "Andrew", "Jim", "Doug", "Chris", "Katie",
+            "Kayla", "Sarah", "Elizabeth", "Stef", "Mary", "Jane", "Kelly", "Jessica", "Beth",
+            "Carla", "Becky", "Linda", "Ruth", "Sandra", "Amy", "Claire", "Donna"};
 
-	//To fill the custom LinkedList
-	private static void generateStudents(int numberOfStudents, MyLinkedList<Student> outList) {
-		Random random = new Random();
-		
-		for(int i = 0; i < numberOfStudents; i++) {
-			int firstNameIndex = random.nextInt(firstNames.length -1);
-			int lastNameIndex = random.nextInt(lastNames.length -1);
-			double gpa = 3.5 * random.nextDouble() + 0.5;
-			outList.add(new Student(firstNames[firstNameIndex], lastNames[lastNameIndex], gpa));	
-		}
-	}
+    private static String[] lastNames = {"Smith", "Davis", "Jones", "Taylor", "Manning", "Jennings", "Williams",
+            "Parker", "Elliot", "Patrick", "Johnson", "Davidson", "O'Connor", "Fritz", "Green",
+            "Gold", "Foley", "Freeman", "Willis", "Grant"};
+
+    public static void main(String[] args) {
+        List<Student> studentArrayList = new ArrayList<>();
+        MyLinkedList<Student> studentLinkedList = new MyLinkedList<>();
+
+        generateStudents(20, studentArrayList);
+        generateStudents(20, studentLinkedList);
+
+        //Determine which data structure is fastest for each
+        //operation.
+        //
+        // Complete the action
+
+        for (int i = 0; i < studentLinkedList.getSize(); i++) {
+            if (studentLinkedList.get(i).getLastName().equalsIgnoreCase("Smith")) {
+                studentLinkedList.remove(i);
+            }
+        }
+
+        for (int i = 0; i < studentArrayList.size(); i++) {
+            if (studentArrayList.get(i).getLastName().equalsIgnoreCase("Smith")) {
+                studentArrayList.remove(i);
+            }
+        }
+
+        studentArrayList.get(2).setFirstName("Joe");
+        studentArrayList.get(2).setLastName("Montana");
+
+        studentLinkedList.get(2).setFirstName("Joe");
+        studentLinkedList.get(2).setLastName("Montana");
+
+
+
+        studentArrayList.remove(9);
+        studentLinkedList.remove(9);
+
+
+        //Delete the first, if any, Student with the last name of "Smith"
+
+        // ANSWER:  An ArrayList will be faster because the get method is quicker in the loop.
+
+        //Change the name of the 3rd entry to "Joe Montana"
+
+        // ANSWER: An ArrayList will be faster because we are going straight to a given index.
+
+        //Remove the 10th element
+
+        // ANSWER: An ArrayList will be faster because we are going straight to an index.
+
+    }
+
+    //To generate the ArrayList
+    private static void generateStudents(int numberOfStudents, List<Student> outList) {
+        Random random = new Random();
+
+        for (int i = 0; i < numberOfStudents; i++) {
+            int firstNameIndex = random.nextInt(firstNames.length - 1);
+            int lastNameIndex = random.nextInt(lastNames.length - 1);
+            double gpa = 3.5 * random.nextDouble() + 0.5;
+            outList.add(new Student(firstNames[firstNameIndex], lastNames[lastNameIndex], gpa));
+        }
+    }
+
+    //To fill the custom LinkedList
+    private static void generateStudents(int numberOfStudents, MyLinkedList<Student> outList) {
+        Random random = new Random();
+
+        for (int i = 0; i < numberOfStudents; i++) {
+            int firstNameIndex = random.nextInt(firstNames.length - 1);
+            int lastNameIndex = random.nextInt(lastNames.length - 1);
+            double gpa = 3.5 * random.nextDouble() + 0.5;
+            outList.add(new Student(firstNames[firstNameIndex], lastNames[lastNameIndex], gpa));
+        }
+    }
 }
--- starter-code/MyLinkedList.java
+++ starter-code/MyLinkedList.java
@@ -1,62 +1,101 @@
-package ListLab;
-
 import java.util.Iterator;
 
-public class MyLinkedList<T> implements Iterable<T>{
+public class MyLinkedList<T> implements Iterable<T> {
 
-	private Node<T> head;
-	private int size = 0;
+    private Node<T> head;
+    private int size = 0;
 
-	public int getSize() {
-		return size;
-	}
+    public int getSize() {
+        return size;
+    }
 
-	public boolean remove(int index) {
-		//to-do
-	}
+    public boolean remove(int index) {
+        if (size == 0) {
+            return false;
+        }
+        if (index > size - 1) {
+            return false;
+        }
+        if (index == 0) {
+            head = head.getNext();
+            return true;
+        }
+        Node prevNode = null;
+        Node currentNode = head;
+        for (int i = 0; i < index; i++) {
+            prevNode = currentNode;
+            currentNode = currentNode.getNext();
+        }
+        prevNode.setNext(currentNode.getNext());
+        return true;
+    }
 
-	public T get(int index) {
-		//to-do
-	}
 
-	public void add(T obj) {
-		//to-do
-	}
+    public T get(int index) {
+        if (size == 0) {
+            return null;
+        }
+        if (index > size - 1) {
+            return null;
+        }
+        Node refNode = head;
+        for (int i = 0; i < index; i++) {
+            refNode = refNode.getNext();
+        }
+        return (T) refNode.getData();
+    }
+    //to-do
 
-	//The methods below are bonus
 
-	public void add(int index, T obj){
-		//to-do
-	}
+    public void add(T obj) {
+        if (head == null) {
+            head = new Node<>(obj);
+        } else {
+            Node refNode = head;
+            while (refNode.getNext() != null) {
+                refNode = refNode.getNext();
+            }
+            refNode.setNext(new Node<>(obj));
+        }
+        size++;
+    }
 
-	@Override
-	public Iterator<T> iterator() {
-		// TODO Auto-generated method stub
-		return new MyIterator<T>(head);
-	}
 
-	private class MyIterator<T> implements Iterator<T> {
+    //The methods below are bonus
 
-		private Node<T> node;
+    public void add(int index, T obj) {
+        //to-do
+    }
 
-		public MyIterator(Node<T> node) {
-			//to-do
-		}
-		@Override
-		public boolean hasNext() {
-			//to-do
-		}
+    @Override
+    public Iterator<T> iterator() {
+        // TODO Auto-generated method stub
+        return new MyIterator<T>(head);
+    }
 
-		@Override
-		public T next() {
-			//to-do
-		}
+    private class MyIterator<T> implements Iterator<T> {
 
-		@Override
-		public void remove() {
-			// to-do
+        private Node<T> node;
 
-		}
+        public MyIterator(Node<T> node) {
+            //to-do
+        }
 
-	}
+        @Override
+        public boolean hasNext() {
+            //to-do
+        }
+
+        @Override
+        public T next() {
+            //to-do
+        }
+
+        @Override
+        public void remove() {
+            // to-do
+
+        }
+
+    }
 }
